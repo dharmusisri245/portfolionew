d3c5bd1aef613228ccf5a07db1d5760622f76da4
Weblius New <dg339736@email.com>
2025-06-22 22:39:16 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,19 +6,19 @@ const About = () => {
     <Layout>
       {/* Background image wrapper */}
       <div
-        className="min-h-screen flex items-center justify-center"
-        style={{
-          backgroundImage: "url('../../public/static/aboutus.avif')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
+        className="min-h-screen flex items-center justify-center bg-gradient-to-r from-black-50 to-blue-300"
+        // style={{
+        //   backgroundImage: "url('../../public/static/aboutus.avif')",
+        //   backgroundSize: "cover",
+        //   backgroundPosition: "center",
+        // }}
       >
         <div className=" flex items-center justify-center p-6">
-          <div className="bg-white rounded-2xl shadow-2xl p-8 max-w-xl w-full">
+          <div className=" bg-amber-200 rounded-2xl shadow-2xl p-8 max-w-xl w-full">
             <h1 className="text-center text-4xl font-extrabold text-gray-800 mb-6">
               About
             </h1>
-            <p className="text-lg text-gray-700 leading-relaxed">
+            <p className="text-lg text-gray-800 leading-relaxed">
               Passionate MERN Stack Developer with expertise in MongoDB,
               Express.js, React, and Node.js. I build fast, scalable web
               applications and love turning ideas into real-world digital
